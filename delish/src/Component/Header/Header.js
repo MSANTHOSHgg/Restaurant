fd70abdfd0f7cc73dfb2bd380ede937165cb5961
Santhosh <msandycrime@gmail.com>
2024-10-23 14:21:11 +0530

    Minor change

--- delish/src/Component/Header/Header.js
+++ delish/src/Component/Header/Header.js
@@ -4,9 +4,7 @@ import {Link} from 'react-router-dom';
 
 import './Header.css'
 const Header = ({menu, setmenu}) => {
-  const refresh=()=>{
-      window.location.reload(false);
-  }
+  
   
   return (
     
--- delish/src/Component/HomeVideo/HomeVideo.js
+++ delish/src/Component/HomeVideo/HomeVideo.js
@@ -1,33 +1,44 @@
 import React from 'react'
 import { useEffect } from 'react'
 import video from '../../assets/delish.mp4'
+import downArrow from '../../assets/arrowd.png'
 import './HomeVideo.css'
+
 export const HomeVideo = () => {
     useEffect(() => {
         const handleScroll = () => {
           const video = document.querySelector('.bgvideo video');
+          const arrow = document.querySelector('.downArrow');
           const scrollY = window.scrollY;
     
           if (scrollY > window.innerHeight * 0.90) {
-            video.style.display = 'none'; // Hide video
+            video.style.display = 'none'; 
+            arrow.style.display= 'none';
           } else {
-            video.style.display = 'block'; // Show video
+            video.style.display = 'block'; 
+            arrow.style.display='block';
           }
         };
     
-        // Attach the scroll event listener
+        
         window.addEventListener('scroll', handleScroll);
     
-        // Clean up the event listener on component unmount
+        
         return () => {
           window.removeEventListener('scroll', handleScroll);
         };
       }, []);
+      const scroll=()=>{
+        window.scrollTo(0,675);
+      }
   return (
-    <div className='bgvideo'>
-        
+    <div>
+      <div className='bgvideo'>
         <video src={video} autoPlay loop muted/>
-        
+    </div>
+    <div className='div_arrow'>
+       <img onClick={scroll} src={downArrow} className='downArrow'/> 
+    </div>
     </div>
   )
 }
